32d493f6b42a82b2c3b00ed29eda796c4271f499
Willian Watanabe <watinha@gmail.com>
2021-04-01 14:19:03 -0300

    includes a single script for running all the experiment

---
+++ run-all.sh
@@ -0,0 +1,89 @@
+rm result/*.csv
+
+python3 main.py games svm 100 3 false
+python3 main.py games svm 300 3 false
+python3 main.py games svm 500 3 false
+python3 main.py games svm 1000 3 false
+python3 main.py games svm 3000 3 false
+python3 main.py games dt 100 3 false
+python3 main.py games dt 300 3 false
+python3 main.py games dt 500 3 false
+python3 main.py games dt 1000 3 false
+python3 main.py games dt 3000 3 false
+
+python3 main.py illiterate svm 100 3 false
+python3 main.py illiterate svm 300 3 false
+python3 main.py illiterate svm 500 3 false
+python3 main.py illiterate svm 1000 3 false
+python3 main.py illiterate svm 3000 3 false
+python3 main.py illiterate dt 100 3 false
+python3 main.py illiterate dt 300 3 false
+python3 main.py illiterate dt 500 3 false
+python3 main.py illiterate dt 1000 3 false
+python3 main.py illiterate dt 3000 3 false
+
+python3 main.py mdwe svm 100 3 false
+python3 main.py mdwe svm 300 3 false
+python3 main.py mdwe svm 500 3 false
+python3 main.py mdwe svm 1000 3 false
+python3 main.py mdwe svm 3000 3 false
+python3 main.py mdwe dt 100 3 false
+python3 main.py mdwe dt 300 3 false
+python3 main.py mdwe dt 500 3 false
+python3 main.py mdwe dt 1000 3 false
+python3 main.py mdwe dt 3000 3 false
+
+python3 main.py ontologies svm 100 3 false
+python3 main.py ontologies svm 300 3 false
+python3 main.py ontologies svm 500 3 false
+python3 main.py ontologies svm 1000 3 false
+python3 main.py ontologies svm 3000 3 false
+python3 main.py ontologies dt 100 3 false
+python3 main.py ontologies dt 300 3 false
+python3 main.py ontologies dt 500 3 false
+python3 main.py ontologies dt 1000 3 false
+python3 main.py ontologies dt 3000 3 false
+
+python3 main.py pair svm 100 3 false
+python3 main.py pair svm 300 3 false
+python3 main.py pair svm 500 3 false
+python3 main.py pair svm 1000 3 false
+python3 main.py pair svm 3000 3 false
+python3 main.py pair dt 100 3 false
+python3 main.py pair dt 300 3 false
+python3 main.py pair dt 500 3 false
+python3 main.py pair dt 1000 3 false
+python3 main.py pair dt 3000 3 false
+
+python3 main.py slr svm 100 3 false
+python3 main.py slr svm 300 3 false
+python3 main.py slr svm 500 3 false
+python3 main.py slr svm 1000 3 false
+python3 main.py slr svm 3000 3 false
+python3 main.py slr dt 100 3 false
+python3 main.py slr dt 300 3 false
+python3 main.py slr dt 500 3 false
+python3 main.py slr dt 1000 3 false
+python3 main.py slr dt 3000 3 false
+
+python3 main.py testing svm 100 3 false
+python3 main.py testing svm 300 3 false
+python3 main.py testing svm 500 3 false
+python3 main.py testing svm 1000 3 false
+python3 main.py testing svm 3000 3 false
+python3 main.py testing dt 100 3 false
+python3 main.py testing dt 300 3 false
+python3 main.py testing dt 500 3 false
+python3 main.py testing dt 1000 3 false
+python3 main.py testing dt 3000 3 false
+
+python3 main.py xbi svm 100 3 false
+python3 main.py xbi svm 300 3 false
+python3 main.py xbi svm 500 3 false
+python3 main.py xbi svm 1000 3 false
+python3 main.py xbi svm 3000 3 false
+python3 main.py xbi dt 100 3 false
+python3 main.py xbi dt 300 3 false
+python3 main.py xbi dt 500 3 false
+python3 main.py xbi dt 1000 3 false
+python3 main.py xbi dt 3000 3 false
