e8ad675b96dc14d7cc949869ab1b2fbf5b53bbe8
Willian Watanabe <watinha@gmail.com>
2021-04-08 18:53:37 -0300

    changing parameters for using abstracts as well

--- run-all-embeddings.sh
+++ run-all-embeddings.sh
@@ -1,26 +1,26 @@
 rm result/*.csv
 
-python3 main.py games embeddings_glove 3 true 20 tfidf
-python3 main.py games embeddings_se 3 true 20 tfidf
+python3 main.py games embeddings_glove 3 false 200 tfidf
+python3 main.py games embeddings_se 3 false 200 tfidf
 
-python3 main.py illiterate embeddings_glove 3 true 20 tfidf
-python3 main.py illiterate embeddings_se 3 true 20 tfidf
+python3 main.py illiterate embeddings_glove 3 false 200 tfidf
+python3 main.py illiterate embeddings_se 3 false 200 tfidf
 
-python3 main.py mdwe embeddings_glove 3 true 20 tfidf
-python3 main.py mdwe embeddings_se 3 true 20 tfidf
+python3 main.py mdwe embeddings_glove 3 false 200 tfidf
+python3 main.py mdwe embeddings_se 3 false 200 tfidf
 
-python3 main.py ontologies embeddings_glove 3 true 20 tfidf
-python3 main.py ontologies embeddings_se 3 true 20 tfidf
+python3 main.py ontologies embeddings_glove 3 false 200 tfidf
+python3 main.py ontologies embeddings_se 3 false 200 tfidf
 
-python3 main.py pair embeddings_glove 3 true 20 tfidf
-python3 main.py pair embeddings_se 3 true 20 tfidf
+python3 main.py pair embeddings_glove 3 false 200 tfidf
+python3 main.py pair embeddings_se 3 false 200 tfidf
 
-python3 main.py slr embeddings_glove 3 true 20 tfidf
-python3 main.py slr embeddings_se 3 true 20 tfidf
+python3 main.py slr embeddings_glove 3 false 200 tfidf
+python3 main.py slr embeddings_se 3 false 200 tfidf
 
-python3 main.py testing embeddings_glove 3 true 20 tfidf
-python3 main.py testing embeddings_se 3 true 20 tfidf
+python3 main.py testing embeddings_glove 3 false 200 tfidf
+python3 main.py testing embeddings_se 3 false 200 tfidf
 
-python3 main.py xbi embeddings_glove 3 true 20 tfidf
-python3 main.py xbi embeddings_se 3 true 20 tfidf
+python3 main.py xbi embeddings_glove 3 false 200 tfidf
+python3 main.py xbi embeddings_se 3 false 200 tfidf
 
--- run-all.sh
+++ run-all.sh
@@ -1,56 +1,56 @@
 rm result/*.csv
 
-python3 main.py games svm 3 true 20 tfidf
-python3 main.py games dt 3 true 20 tfidf
+python3 main.py games svm 3 false 20 tfidf
+python3 main.py games dt 3 false 20 tfidf
 python3 main.py games svm 3 false 20 embeddings_glove
 python3 main.py games svm 3 false 20 embeddings_se
 python3 main.py games dt 3 false 20 embeddings_glove
 python3 main.py games dt 3 false 20 embeddings_se
 
-python3 main.py illiterate svm 3 true 20 tfidf
-python3 main.py illiterate dt 3 true 20 tfidf
+python3 main.py illiterate svm 3 false 20 tfidf
+python3 main.py illiterate dt 3 false 20 tfidf
 python3 main.py illiterate svm 3 false 20 embeddings_glove
 python3 main.py illiterate svm 3 false 20 embeddings_se
 python3 main.py illiterate dt 3 false 20 embeddings_glove
 python3 main.py illiterate dt 3 false 20 embeddings_se
 
-python3 main.py mdwe svm 3 true 20 tfidf
-python3 main.py mdwe dt 3 true 20 tfidf
+python3 main.py mdwe svm 3 false 20 tfidf
+python3 main.py mdwe dt 3 false 20 tfidf
 python3 main.py mdwe svm 3 false 20 embeddings_glove
 python3 main.py mdwe svm 3 false 20 embeddings_se
 python3 main.py mdwe dt 3 false 20 embeddings_glove
 python3 main.py mdwe dt 3 false 20 embeddings_se
 
-python3 main.py ontologies svm 3 true 20 tfidf
-python3 main.py ontologies dt 3 true 20 tfidf
+python3 main.py ontologies svm 3 false 20 tfidf
+python3 main.py ontologies dt 3 false 20 tfidf
 python3 main.py ontologies svm 3 false 20 embeddings_glove
 python3 main.py ontologies svm 3 false 20 embeddings_se
 python3 main.py ontologies dt 3 false 20 embeddings_glove
 python3 main.py ontologies dt 3 false 20 embeddings_se
 
-python3 main.py pair svm 3 true 20 tfidf
-python3 main.py pair dt 3 true 20 tfidf
+python3 main.py pair svm 3 false 20 tfidf
+python3 main.py pair dt 3 false 20 tfidf
 python3 main.py pair svm 3 false 20 embeddings_glove
 python3 main.py pair svm 3 false 20 embeddings_se
 python3 main.py pair dt 3 false 20 embeddings_glove
 python3 main.py pair dt 3 false 20 embeddings_se
 
-python3 main.py slr svm 3 true 20 tfidf
-python3 main.py slr dt 3 true 20 tfidf
+python3 main.py slr svm 3 false 20 tfidf
+python3 main.py slr dt 3 false 20 tfidf
 python3 main.py slr svm 3 false 20 embeddings_glove
 python3 main.py slr svm 3 false 20 embeddings_se
 python3 main.py slr dt 3 false 20 embeddings_glove
 python3 main.py slr dt 3 false 20 embeddings_se
 
-python3 main.py testing svm 3 true 20 tfidf
-python3 main.py testing dt 3 true 20 tfidf
+python3 main.py testing svm 3 false 20 tfidf
+python3 main.py testing dt 3 false 20 tfidf
 python3 main.py testing svm 3 false 20 embeddings_glove
 python3 main.py testing svm 3 false 20 embeddings_se
 python3 main.py testing dt 3 false 20 embeddings_glove
 python3 main.py testing dt 3 false 20 embeddings_se
 
-python3 main.py xbi svm 3 true 20 tfidf
-python3 main.py xbi dt 3 true 20 tfidf
+python3 main.py xbi svm 3 false 20 tfidf
+python3 main.py xbi dt 3 false 20 tfidf
 python3 main.py xbi svm 3 false 20 embeddings_glove
 python3 main.py xbi svm 3 false 20 embeddings_se
 python3 main.py xbi dt 3 false 20 embeddings_glove
